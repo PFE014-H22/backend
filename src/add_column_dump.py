ee200d9c37597d4258b2178fda21c2e71a137705
gabrielsomma <gabriel.somma@live.ca>
2022-10-18 10:50:05 -0400

    tf-idf module prep

--- src/add_column_dump.py
+++ src/add_column_dump.py
@@ -10,5 +10,14 @@ headers = next(reader)
 headers.append("vecteur tf-idf")
 print(headers)
 writer.writerow(headers)
+
 for row in reader:
+    # id = row[0]
+    # ici on aurait un call vers le module tf-idf avec le id en param
+    # vector = tfidf.getVector(id)
+    # apres on append dans chaque row pour l'ecrire dans la derniere colonne
+    # row.append(vector)
+    
+    # row.append(id) # un test pour s'assurer que ca retourne la bonne info
+
     writer.writerow(row)
