c72db96acdd3687033ce04cc1274b214c73bf29b
Jérémy Lalonde <jeremy.lalonde.1@ens.etsmtl.ca>
2022-10-25 17:14:37 -0400

    Add doc and find_parameter test

--- config_parameters/cassandra/fetch_cassandra_parameters.py
+++ config_parameters/cassandra/fetch_cassandra_parameters.py
@@ -3,6 +3,12 @@ import requests
 
 
 def fetch_params():
+    """
+    Fetches all the parameters from the cassandra documentation
+
+    Returns:
+        set(str): set of strings containing all the parameters
+    """
     config_parameters = set()
 
     url = "https://cassandra.apache.org/doc/latest/cassandra/configuration/cass_yaml_file.html"
@@ -21,18 +27,37 @@ def fetch_params():
     html = get_html_from(url=url)
     extract_code_from(config_parameters, html, "h3")
 
-    write_to_file("./cassandra_parameters.txt", "\n".join(config_parameters))
+    return "\n".join(config_parameters)
 
 
 def get_html_from(url: str):
+    """
+    Gets the html from the url
+
+    Args:
+        url (str): url to fetch the html from
+
+    Returns:
+        str: html from the url
+    """
     page = requests.get(url)
     return BeautifulSoup(page.content, "html.parser")
 
 
-def extract_code_from(response_set, html, title_tag="h2"):
-    h2_list = html.find_all(title_tag)
-    for h2 in h2_list:
-        title = h2.find("code")
+def extract_code_from(response_set, html, title_tag):
+    """
+    Extracts the parameters from the html
+    On the cassandra documentation site, all parameters are in code tags
+    and are either in h2 or h3 tags
+
+    Args:
+        response_set (str): set to which the parameters are added
+        html (str): content of the html
+        title_tag (str): Tag in which the parameter is in for this specific documentation page
+    """
+    tags = html.find_all(title_tag)
+    for parameter_title in tags:
+        title = parameter_title.find("code")
         if title is None:
             continue
 
@@ -40,9 +65,36 @@ def extract_code_from(response_set, html, title_tag="h2"):
 
 
 def write_to_file(file_name, content):
+    """
+    Writes the content to the file
+
+    Args:
+        file_name (str): path to the file to be written to
+        content (str): content to be written to the file
+    """
     with open(file_name, "w") as f:
         f.write(content)
 
 
+def find_parameter(text: str, parameter_file: str) -> list:
+    """
+    Finds all the parameters of the file present in the text
+
+    Args:
+        text (str): text to be searched
+        parameter_file (str): file containing the parameters
+
+    Returns:
+        list[str]: list of parameters present in the text
+    """
+    with open(parameter_file, "r") as f:
+        parameters = f.read().splitlines()
+
+    return list(set(text.split(" ")).intersection(parameters))
+
 if __name__ == "__main__":
-    fetch_params()
+    """
+    File to run to populate the parameters file with the parameters from the cassandra documentation
+    """
+    config_parameters = fetch_params()
+    write_to_file("./cassandra_parameters.txt", config_parameters)
---
+++ config_parameters/cassandra/test.py
@@ -0,0 +1,40 @@
+
+import unittest
+
+from fetch_cassandra_parameters import find_parameter
+
+
+class TestFindParameters(unittest.TestCase):
+
+    text = """
+    cluster_name Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in felis turpis. Suspendisse hendrerit imperdiet consectetur. Quisque mi turpis, rutrum malesuada purus posuere, mattis imperdiet dui. Praesent dignissim ante nibh, a eleifend sem efficitur ac. Mauris sollicitudin, nulla sed pulvinar rhoncus, arcu ipsum eleifend odio, vel pretium erat felis ac ex. Integer nisi quam, finibus ac erat nec, congue mattis justo. Aenean eget cursus nibh. Nulla maximus mauris sit amet nunc tristique, euismod aliquam leo interdum. Aliquam egestas auctor nisl. Sed porttitor urna sit amet tortor luctus placerat.
+
+    Cras quis aliquet justo, vel auctor ex. Donec tincidunt felis turpis, nec tincidunt nunc auctor eget. Sed tincidunt malesuada dolor, nec posuere lectus. Etiam egestas scelerisque enim, ac tincidunt tellus dictum id. Sed a elementum mauris. Nulla pretium sapien eu tempus volutpat. Donec fermentum in dui aliquet hendrerit. Fusce placerat, libero ut commodo ultrices, mi leo molestie arcu, sed convallis lorem purus a massa. Sed aliquam sapien turpis, at aliquam mi facilisis a. Sed tincidunt, lorem sed ullamcorper euismod, leo metus laoreet erat, ac sodales augue metus vel erat. Ut quis massa eu tellus sagittis aliquet ut a nulla. Morbi dictum, justo ac pellentesque porttitor, eros leo consequat tortor, ut ultrices elit mauris sed massa. Duis nec massa vel nulla vestibulum tincidunt.
+
+    Aliquam ut ex lectus. Phasellus scelerisque elementum rhoncus. Suspendisse potenti. Duis finibus finibus ultricies. Morbi nec sagittis purus. Ut aliquet sodales leo vitae ornare. Nam vulputate pharetra dui, id faucibus mi efficitur ut. Vestibulum ac lectus in massa euismod vehicula malesuada in erat. In finibus orci quis dui consequat pellentesque. Nulla ipsum est, dignissim sed hendrerit sed, convallis sed nisl. Quisque a tristique turpis. Phasellus scelerisque eros sit amet ipsum sollicitudin lobortis. Integer dictum ligula at dapibus egestas. Integer eu nibh quis lectus cursus semper. Nunc volutpat dui quis metus cursus, et sodales lectus cursus.
+
+    Aliquam aliquet aliquam justo, eget vehicula libero aliquet vitae. Curabitur max_hints_delivery_threads imperdiet odio id est condimentum, in auctor tortor porttitor. Suspendisse nisi ligula, tristique cursus porttitor et, efficitur et nisi. Maecenas efficitur scelerisque arcu, ut convallis orci rhoncus a. Proin elit elit, efficitur aliquet consectetur et, lacinia nec metus. Aliquam erat volutpat. In est eros, ornare vitae elit non, ornare elementum magna. Vestibulum convallis bibendum sem, non ornare arcu dictum facilisis. Vivamus eget malesuada quam.
+
+    Vestibulum tincidunt neque a massa fringilla condimentum. Donec ac ultricies erat. Sed commodo et nunc nec placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas quis lectus eu elit imperdiet prefer_local tempor a eu justo. Aliquam rhoncus lobortis metus, at iaculis nulla auctor eu. Nulla eleifend blandit nulla. Duis vitae sem eu diam feugiat molestie. Proin porttitor auctor rhoncus. Nam tempor volutpat dapibus. Sed diam nulla, tincidunt a pharetra iaculis, malesuada ac orci. Nam sollicitudin justo vel pulvinar condimentum. Nunc congue quam a est vehicula, in dignissim libero consectetur
+    """
+
+    def test_isAnagrame(self):
+        expected_parameter = [
+            'prefer_local',
+            'cluster_name',
+            'max_hints_delivery_threads',
+        ]
+
+        parameters_found = find_parameter(
+            text=self.text,
+            parameter_file="./cassandra_parameters.txt"
+        )
+
+        self.assertEqual(
+            sorted(expected_parameter),
+            sorted(parameters_found)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
